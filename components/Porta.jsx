8a795382cb4d01c1c3017574a6ab3b0ad8d9cbda
Eduardo Ruiz <eduruiz333@gmail.com>
2022-01-31 16:25:56 -0300

    Teste de push

--- components/Porta.jsx
+++ components/Porta.jsx
@@ -4,6 +4,7 @@ export default function Porta(props) {
     return (
         <div className={styles.porta}>
             <div className={styles.numero}>3</div>
+            <div className={styles.numero}>3</div>
         </div>
     )
 }
